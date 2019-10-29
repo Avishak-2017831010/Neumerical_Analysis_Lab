9fd8c0a56dee9e4fa7fb867af56082f296c409d6
Avishak Chakroborty <aurgho1998@gmail.com>
2019-10-29 19:51:39 +0600

    Add files via upload

--- FalsePosition.m
+++ FalsePosition.m
@@ -1,28 +1,18 @@
-## Copyright (C) 2019 Avi
-## 
-## This program is free software: you can redistribute it and/or modify it
-## under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
-## 
-## This program is distributed in the hope that it will be useful, but
-## WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-## 
-## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see
-## <https://www.gnu.org/licenses/>.
-
-## -*- texinfo -*- 
-## @deftypefn {} {@var{retval} =} FalsePosition (@var{input1}, @var{input2})
-##
-## @seealso{}
-## @end deftypefn
-
-## Author: Avi <Avi@DESKTOP-DMLDNP2>
-## Created: 2019-10-28
-
-function retval = FalsePosition (input1, input2)
-
+function value=BisFunc(f,a,b);
+  pkg load symbolic;
+  syms x;
+  func=inline(f);
+d = 1e-10;
+for i=1:10
+    m = (a*func(b)-b*func(a))/func(b)-func(a);
+    printf("%d %d %d %d %d %d\n",a,b,func(a),func(b),m,func(m));
+    
+    if func(m)*func(a) < 0
+        b = m;
+    else
+        a = m;
+  endif
+  
+endfor
+value=m;
 endfunction
